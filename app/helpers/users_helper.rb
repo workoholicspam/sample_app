05c90cd40983bb8b3cbd40921012fc3022e8f4bf
workoholic <workoholicspam@gmail.com>
2012-08-04 15:48:52 -0700

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user)
+    email_digest = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{email_digest}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
