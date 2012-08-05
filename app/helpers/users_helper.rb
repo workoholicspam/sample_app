013535c090249d77ec1264dea749f6bf19fb3121
workoholic <workoholicspam@gmail.com>
2012-08-04 17:34:21 -0700

    Complete chapter 7 exercise

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,9 @@
 module UsersHelper
-  def gravatar_for(user)
-    email_digest = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{email_digest}"
+  def gravatar_for(user, option = {size: 50})
+    id = Digest::MD5::hexdigest(user.email.downcase)
+    sz = option[:size]
+
+    gravatar_url = "https://secure.gravatar.com/avatar/#{id}?s=#{sz}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,12 +18,6 @@ describe "User Pages" do
     it { should have_selector('title',  text:full_title('Sign up')  )}
   end
 
-
-
-
-
-
-
   describe "signup" do
 
     before { visit signup_path }
@@ -33,6 +27,13 @@ describe "User Pages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+        
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content ('error') }
       end
     end
 
@@ -47,16 +48,17 @@ describe "User Pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text:'Welcome')}
+      end
+
+
     end
   end
-
-
-
-
-
-
-
-
-
-
 end
